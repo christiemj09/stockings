bdef7269cc33ef37253ff48842eed2c4ee133b82
Ethan Nelson <git@ethan-nelson.com>
2018-02-06 21:26:13 -0600

    Overhaul reader

--- stockings/reader.py
+++ stockings/reader.py
@@ -1,6 +1,7 @@
 import datetime
 import glob
 import os
+import re
 
 def get_stocks(path):
     """Finds comma-separated values in a path and, assuming they are
@@ -11,7 +12,7 @@ def get_stocks(path):
     """
     filenames = glob.glob('%s/*.csv' % (path, ))
     # TODO: verify naming scheme of CSV files (I don't have access RN)
-    return {os.path.basename(x).split('.')[1]: x for x in filenames}
+    return {os.path.basename(x).split('.')[0]: x for x in filenames}
 
 
 def read_stock(path):
@@ -25,31 +26,60 @@ def read_stock(path):
     f.close()
 
     stock = {}
-    if 'Key Statistics' in data[3]:
-        stock['name'] = data[0][0].split('-')[0]
-        stock['industry'] = data[1][1]
-        stock['sector'] = data[1][2]
-        stock['keystats'] = {}
-        for line in data[4:10]:
-            stock['keystats'][line[0]] = line[1]
-        stock['growthrates'] = {}
-        for line in data[12:17]:
-            stock['growthrates'][line[0]] = line[1]
-
-        stock['annual'] = {}
-        stock['annual']['dates'] = [datetime.strptime('%b%Y', x) for x in data[20][1:31]]
-        # Right now we skip the TTM/current since it doesn't follow the same month.
-        #   To include it, uncomment the immedately following line and change the
-        #   loop under it to extend to 32.
-        #stock['annual']['dates'].append(datetime.strptime('%b%Y', 'Jan2017'))
-        for line in data[21:]:
-            stock['annual'][line[0]] = [float(x) for x in line[1:31]]
-
-        stock['quarterly'] = {}
-        stock['quarterly']['dates'] = [datetime.strptime('%b%Y', x) for x in data[20][33:]]
-        for line in data[21]:
-            stock['quarterly'][line[0]] = [float(x) for x in line[33:]]
-
-        return stock
-    else:
+    start = [ix for ix,x in enumerate(data) if 'Key Statistics' in x]
+    if len(start) != 1:
         raise Exception('File format not recognized or Ethan is bad.')
+    start = int(start[0])
+
+    stock['name'] = data[0][0].split('-')[0].strip()
+    stock['industry'] = data[1][1]
+    stock['sector'] = data[1][2]
+    stock['keystats'] = {}
+    for line in data[start+1:start+7]:
+        stock['keystats'][line[0]] = line[1]
+    stock['growthrates'] = {}
+    for line in data[start+9:start+14]:
+        stock['growthrates'][line[0]] = line[1]
+
+    floats = re.compile(u"^[-]?[0-9]+.[0-9]+$").search
+    dates = re.compile(u"^[0-9]+/[0-9]+/[0-9]+$").search
+
+    stock['annual'] = {}
+    stock['annual']['dates'] = [datetime.datetime.strptime(x.strip('\r\n'), '%b%Y') for x in data[start+17][1:31]]
+    # Right now we skip the TTM/current since it doesn't follow the same month.
+    #   To include it, uncomment the immedately following line and change the
+    #   loop under it to extend to 32.
+    #stock['annual']['dates'].append(datetime.strptime('%b%Y', 'Jan2017'))
+    for line in data[start+18:]:
+        lstart = None
+        for iv,value in enumerate(line):
+            if floats(value):
+                lstart = iv
+                break
+        if lstart:
+            stock['annual'][line[0]] = [float(x.strip('\r\n')) if floats(x) else float('nan') for x in line[lstart:lstart+30]]
+        else:
+            # FIXME: handle data types besides floats; for now we ignore
+            continue
+
+    for key in stock['annual'].keys():
+        assert len(stock['annual'][key]) == len(stock['annual']['dates'])
+
+    stock['quarterly'] = {}
+    stock['quarterly']['dates'] = [datetime.datetime.strptime(x.strip('\r\n'), '%b%Y') for x in data[start+17][33:]]
+    for line in data[start+18:]:
+        lstart = None
+        for iv,value in enumerate(line):
+            if floats(value):
+                lstart = iv
+                break
+        if lstart:
+            stock['quarterly'][line[0]] = [float(x.strip('\r\n')) if floats(x) else float('nan') for x in line[lstart+32:]]
+        else:
+            # FIXME: handle data types besides floats; for now we ignore
+            continue
+
+    for key in stock['quarterly'].keys():
+         assert len(stock['quarterly'][key]) == len(stock['quarterly']['dates'])
+
+    return stock
